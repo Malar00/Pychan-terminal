c5e523f59eddb8e843b56dfec39b1377f3c8fe99
Jan Sitarski <jansitarski00@gmail.com>
2020-08-24 18:41:41 +0200

    working catalog

--- main.py
+++ main.py
@@ -14,30 +14,27 @@ def get_boards():
         json.dump(response_json, file)
 
 
-def get_threads(board):
-    response = requests.get('https://a.4cdn.org/' + board + '/threads.json')
+def get_catalog(board):
+    response = requests.get('https://a.4cdn.org/' + board + '/catalog.json')
     response_json = response.json()
-    with open('4chan_threads.json', 'w') as file:
+    with open('4chan_catalog.json', 'w') as file:
         json.dump(response_json, file)
 
 
 def list_threads():
-    data = read_json("4chan_threads.json")
+    data = read_json("4chan_catalog.json")
     thread_list = []
-    for threads in data:
-        for thread_number in threads['threads']:
-            print(thread_number['no'])
-            thread_list.append(
-                requests.get(
-                    'https://a.4cdn.org/' + current_board + '/thread/' + str(thread_number['no']) + '.json'))
-        break
-
-    for threads in thread_list:
-        for thread in threads.json()['posts']:
+    for catalog in data:
+        for thread in catalog['threads']:
             try:
-                print(str(thread['no']))
+                print("No." + str(thread['no']))
             except KeyError:
                 print("no number")
+
+            try:
+                print(str(thread['sub']))
+            except KeyError:
+                print("no subject")
 
             try:
                 h = html2text.HTML2Text()
@@ -47,6 +44,30 @@ def list_threads():
 
             for width in range(os.get_terminal_size()[0]):
                 print("-", end='')
+
+'''
+    for posts in thread_list:
+        for post in posts.json()['posts']:
+            if 'unique_ips' in post:
+                try:
+                    print(str(post['no']))
+                except KeyError:
+                    print("no number")
+
+                try:
+                    print("[ replies : " + str(post['unique_ips']) + " ]")
+                except KeyError:
+                    print("not a thread")
+
+                try:
+                    h = html2text.HTML2Text()
+                    print(h.handle(post['com']))
+                except KeyError:
+                    print("no com")
+
+                for width in range(os.get_terminal_size()[0]):
+                    print("-", end='')
+'''
 
 
 def list_boards():
@@ -62,7 +83,7 @@ def list_boards():
 
 def board_choose(board_list):
     choice = int(input("choose a board from [0-" + str(len(board_list) - 1) + "]: "))
-    get_threads(board_list[choice])
+    get_catalog(board_list[choice])
     global current_board
     current_board = board_list[choice]
 
